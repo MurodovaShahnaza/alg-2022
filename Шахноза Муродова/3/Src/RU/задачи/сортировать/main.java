9e3f40b245dbbf21340503132369dfc9d732a581
Pororo <99640905+MurodovaShahnaza@users.noreply.github.com>
2022-05-15 08:06:13 +0300

    Update main.java

--- Шахноза Муродова/3/Src/RU/задачи/сортировать/main.java
+++ Шахноза Муродова/3/Src/RU/задачи/сортировать/main.java
@@ -1,69 +1,128 @@
-package ru.tasks.sort;
+import java.util.*;
+import java.lang.*;
 
-import java.util.Arrays;
-import java.util.Random;
-
-public class Main {
- 
-    public static void main(String [] args) {
-        int [][] array1 = {
-                {9, 0, -2, 89, 1, 2, 3, -3, -99, 6},
-                {5, 3, 1, 2, 2, 1, 0},
-                {1, 1},
-                {3, 1, 1},
-                {2, 1, 1, 2, 1, 2},
-                {2, 5, 3, 1},
-                {-1, 3, 2, 4, 0, 1},
-                {-1, 0}
-                };
-        sortAndPrintln(array1);
-    }
+class Rextester
+{  
     
-    public static void sortAndPrintln(int[][] array1) {
-        for (int n = 0; n < array1.length; n++) {
-            quicksort(array1[n], 0, array1[n].length - 1);
-            System.out.println(Arrays.toString(array1[n]));
-        }
+    public static void main(String args[]) {
+        int[] arr = { 3,6,1,0,2,8,10,4,9,5,7,89,4,56,78,9,32,45,76,87,15,68,92,83,81,123,84}; 
+        bubbleSort(arr);
+        for (int h = 0; h < arr.length; h++)
+            System.out.print(arr[h] + " ");
     }
-  
-    public static void quicksort(int[] array1, int startIndex, int endIndex) {
-        int pivotValue = getPivot(array1, startIndex, endIndex);
-        int currentStartIndex = startIndex;
-        int currentEndIndex = endIndex;
-        
-        while (currentStartIndex < currentEndIndex) {
-            while (array1[currentStartIndex] < pivotValue) {
-                currentStartIndex++;
-            } 
-            while ((array1[currentEndIndex] > pivotValue) && (currentEndIndex > currentStartIndex)) {
-                currentEndIndex--;
-            }
-            if (currentStartIndex < currentEndIndex) {
-                swap(array1, currentStartIndex, currentEndIndex);
-                if (currentEndIndex - currentStartIndex > 1) {
-                    currentStartIndex++;
-                    currentEndIndex--;
-                } else {
-                    break;
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        if ((currentStartIndex > startIndex)
-                && (currentStartIndex - startIndex > 1))
-            quicksort(array1, startIndex, currentStartIndex);
-        if ((endIndex > currentEndIndex)
-                && (endIndex - currentEndIndex > 1))
-            quicksort(array1, currentEndIndex , endIndex);
     }
-    
-    
-    private static void swap(int[] array1, int index1, int index2) {
-        int buffer = array1[index1];
-        array1[index1] = array1[index2];
-        array1[index2] = buffer;
+     public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int value = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > value) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = value;
+        }
     }
+     
+    public static void ShellSort(int[] arr) {
+        int increment = arr.length / 2;
+        while (increment > 0) {
+            for (int i = increment; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[i];
+                while (j >= increment && arr[j - increment] > temp) {
+                    arr[j] = arr[j - increment];
+                    j = j - increment;
+                }
+                arr[j] = temp;
+            }
+            if (increment == 2) {
+                increment = 1;
+            } else {
+                increment *= (5.0 / 11);
+            }
+        }
+    }
+    public static void BucketSort(int[]arr)
+    {     int j2,j3;
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 0; i < arr.length ; i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+            }
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }   
+        }
+    
+        int[] bucket1 = new int[arr.length]; 
+        for (int i = 0; i < arr.length; i++) 
+        bucket1[i] = 88; 
+    
+        int[] bucket2 = new int[arr.length]; 
+        for (int i = 0; i < arr.length; i++) 
+        bucket2[i] = 88; 
+
+    
+        int range = ((max - min)/2) + 1;
+       
+        int j = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if((arr[i] >= min) && (arr[i] < min + range))
+            {
+                bucket1[j] = arr[i];
+                j ++;
+            }
+        }
+       
+        j2 = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if((arr[i] >= min + range) && (arr[i] <= max))
+            {
+                bucket2[j2] = arr[i];
+                j2 ++;
+            }
+        }
+       
+       
+        insertionSort(bucket1);
+       
+      ShellSort(bucket2);
+        
   
-  public static int getPivot(int[] array1, int startIndex, int endIndex) {
-        return array1[endIndex - 1];
-    }
+    j =0;
+     j2=0;
+    
+for (int i=0; i< arr.length; i++)
+{ 
+if(bucket1[j]<88){
+    arr[i]=bucket1[j]; 
+j++;} 
+else{if(bucket2[j2]<88){ 
+arr[i]=bucket2[j2]; 
+j2++;} 
+
+
+    }}
+     
+    System.out.println("Вывод отсортированного массива"); 
+System.out.println(Arrays.toString(arr)); 
+}
 }
