9e87c6a70b1f52ccf670974a5393761aed1c563e
Муродова Шахноза <99640905+MurodovaShahnaza@users.noreply.github.com>
2022-05-15 06:12:48 +0300

    Add files via upload

---
+++ Шахноза Муродова/2/Src/RU/bgpu/задача/deque/ideque.java
@@ -0,0 +1,21 @@
+public class ideque {
+    package ru.bgpu.task.deque;
+
+    public interface IDeque<T> {
+        boolean isEmpty();
+
+        boolean isFull();
+
+        void pushBack(T value);
+
+        T popBack();
+
+        T peekBack();
+
+        void pushFront(T value);
+
+        T popFront();
+
+        T peekFront();
+    }
+}
