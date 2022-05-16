3c9322ad43cd5753e94144929b7b3ccdfe38e94f
Pororo <99640905+MurodovaShahnaza@users.noreply.github.com>
2022-05-16 10:36:27 +0300

    Update main.java

--- Шахноза Муродова/6/RU/задачи/Карта/main.java
+++ Шахноза Муродова/6/RU/задачи/Карта/main.java
@@ -1,4 +1,11 @@
-public class main {
+package ru.tasks.map;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
 
         public static void main(String[] args) {
             JFileChooser fileChooser = new JFileChooser();
